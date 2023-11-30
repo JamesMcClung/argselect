9cd9f6dcaa50a1bf56332181289c0dd3cca51155
James McClung <james@themcclungs.net>
2023-11-29 20:48:24 -0500

    extension: -disposable var

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "argselect" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('argselect.helloWorld', argselect.helloWorld);
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('argselect.helloWorld', argselect.helloWorld));
 }
 
 // This method is called when your extension is deactivated
