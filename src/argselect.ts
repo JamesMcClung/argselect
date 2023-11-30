c3157f69d20b2eeb2e2b9afa96e2f7260358f28c
James McClung <james@themcclungs.net>
2023-11-29 21:25:15 -0500

    argselect: +expandSelection command
    doesn't do the right thing yet

--- src/argselect.ts
+++ src/argselect.ts
@@ -3,3 +3,19 @@ import * as vscode from 'vscode';
 export function helloWorld() {
     vscode.window.showInformationMessage('Hello World 2 from argselect!');
 }
+
+function expandSelection(doc: vscode.TextDocument, sel: vscode.Selection): vscode.Selection {
+    const anchorPos = doc.lineAt(sel.start.line).range.start;
+    const activePos = doc.lineAt(sel.end.line).range.end;
+    return new vscode.Selection(anchorPos, activePos);
+}
+
+export function selectArg() {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+        return;
+    }
+
+    editor.selections = editor.selections.map(sel => expandSelection(editor.document, sel));
+}
