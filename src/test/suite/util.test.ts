e0ffd6f6c862266b9539cfc660409217c5b95d59
James McClung <james@themcclungs.net>
2023-12-02 01:57:31 -0500

    util.test: +traverseUntilOutOfString

--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -24,4 +24,32 @@ suite('Extension Test Suite', () => {
         assert.strictEqual(true, util.isEscaped(String.raw`"\\\""`, 4));
         assert.strictEqual(false, util.isEscaped(String.raw`"\\\""`, 5));
     });
+
+    test('traverseUntilOutOfString', () => {
+        assert.strictEqual(undefined, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 3, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 4, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 5, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 6, -1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 4, 1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 5, 1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 6, 1, '"'));
+        assert.strictEqual(undefined, util.traverseUntilOutOfString(String.raw`0, "45", 9`, 7, 1, '"'));
+
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 4, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 5, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 6, -1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 4, 1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 5, 1, '"'));
+        assert.strictEqual(7, util.traverseUntilOutOfString(String.raw`0, "\"", 9`, 6, 1, '"'));
+
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "'", 8`, 4, -1, '"'));
+        assert.strictEqual(2, util.traverseUntilOutOfString(String.raw`0, "'", 8`, 5, -1, '"'));
+        assert.strictEqual(6, util.traverseUntilOutOfString(String.raw`0, "'", 8`, 4, 1, '"'));
+        assert.strictEqual(6, util.traverseUntilOutOfString(String.raw`0, "'", 8`, 5, 1, '"'));
+
+        assert.strictEqual(0, util.traverseUntilOutOfString('0"2\n4"6', 3, -1, '"'));
+        assert.strictEqual(undefined, util.traverseUntilOutOfString('0"2\n4"6', 4, -1, '"'));
+        assert.strictEqual(undefined, util.traverseUntilOutOfString('0"2\n4"6', 3, 1, '"'));
+        assert.strictEqual(6, util.traverseUntilOutOfString('0"2\n4"6', 4, 1, '"'));
+    });
 });
