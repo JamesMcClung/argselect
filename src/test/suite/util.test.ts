7fb12afd8f516a41da04b79990a15c21ca20a61d
James McClung <james@themcclungs.net>
2023-12-02 03:19:41 -0500

    util.test: +getCurrentStringType

--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -52,4 +52,48 @@ suite('Extension Test Suite', () => {
         assert.strictEqual(undefined, util.traverseUntilOutOfString('0"2\n4"6', 3, 1, '"'));
         assert.strictEqual(6, util.traverseUntilOutOfString('0"2\n4"6', 4, 1, '"'));
     });
+
+    test('getCurrentStringType', () => {
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"123"`, 0));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"123"`, 1));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"123"`, 2));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"123"`, 3));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"123"`, 4));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"123"`, 5));
+
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1\""`, 0));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1\""`, 1));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1\""`, 2));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1\""`, 3));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1\""`, 4));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1\""`, 5));
+
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1"3'5'`, 0));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1"3'5'`, 1));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"1"3'5'`, 2));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1"3'5'`, 3));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1"3'5'`, 4));
+        assert.strictEqual("'", util.getCurrentStringType(String.raw`"1"3'5'`, 5));
+        assert.strictEqual("'", util.getCurrentStringType(String.raw`"1"3'5'`, 6));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"1"3'5'`, 7));
+
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"12'45"`, 0));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 1));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 2));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 3));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 4));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 5));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'45"`, 6));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"12'45"`, 7));
+
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"12'4'6"`, 0));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 1));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 2));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 3));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 4));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 5));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 6));
+        assert.strictEqual('"', util.getCurrentStringType(String.raw`"12'4'6"`, 7));
+        assert.strictEqual(undefined, util.getCurrentStringType(String.raw`"12'4'6"`, 8));
+    });
 });
