5cdfd112d46b8461a9fb34578a733220528d891e
James McClung <james@themcclungs.net>
2023-12-30 14:04:59 -0500

    util: record lastNonSpace on string exit

--- src/util.ts
+++ src/util.ts
@@ -143,6 +143,7 @@ export function traverseUntilUnmatchedParen(
                 return undefined;
             }
             i = stringExit - dir;
+            lastNonSpace = i;
             continue;
         }
 
